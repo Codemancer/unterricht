c1065daf9c2551a824a10c33b910104649bf67a8
Enrico Pattberg <codemancer86@googlemail.com>
2019-11-11 10:31:07 +0100

    Test

--- MasterLearner/src/Model/QuestionItem.java
+++ MasterLearner/src/Model/QuestionItem.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 //Test
 // Das hier ist noch eine Änderung
+// Das hier ist noch eine Änderung
 public class QuestionItem {
 	private String mstrQuestion;
 	public String getQuestion() { return mstrQuestion; }
